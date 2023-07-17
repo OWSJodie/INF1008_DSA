ed5071d30230dece334b7b244695060a33225b7e
Jodie Ong <113889432+OWSJodie@users.noreply.github.com>
2023-07-17 18:25:13 +0800

    Update index.py
    process the data and store it global, when route to other page it will retain the data.

--- index.py
+++ index.py
@@ -1,19 +1,17 @@
-from flask import Flask
-from flask import render_template
-from flask import redirect
-from flask import request
-
-import pandas as pd
 import json
+
 import plotly
-import plotly.express as px
 import plotly.graph_objs as go
 from cybernews.cybernews import CyberNews
+from flask import Flask
+from flask import redirect
+from flask import render_template
+
 import algorithm.analysis as analysis
 
 app = Flask(__name__)
 
-file_location = 'dataset/updated_cve_2023_07_09.xlsx'
+file_location = 'dataset/updated_cve_2023_07_09.csv'
 
 mapping = {
     'LOW': 2.5,
@@ -22,7 +20,6 @@ mapping = {
     'CRITICAL': 10
 }
 
-global df, filtered_df
 df = analysis.load_data(file_location)
 df = analysis.map_words_to_values(df, 'Mixed_baseSeverity', mapping)
 
@@ -31,39 +28,16 @@ variables_of_interest = ['Mixed_exploitabilityScore',
                          'Mixed_baseSeverity',
                          'Mixed_basedScore']
 
-# Remove all score which is 0 or na
 filtered_df = df[(df[variables_of_interest] != 0).dropna().all(axis=1)]
+
+# Store analysis results globally
+global analysis_results
+analysis_results = None
 
 
 @app.route('/')
 def index():
     return redirect("/analytics", code=302)
-
-
-# @app.route('/analytics')
-# def analytics():
-#     test_data = [
-#         {"RansomAttack": "WannaCry", "Industry": "Education", "average_cost": 5},
-#         {"RansomAttack": "Locky", "Industry": "Education", "average_cost": 10},
-#         {"RansomAttack": "Ryuk", "Industry": "Education", "average_cost": 8},
-#         {"RansomAttack": "WannaCry", "Industry": "Health", "average_cost": 3},
-#         {"RansomAttack": "Locky", "Industry": "Health", "average_cost": 4},
-#         {"RansomAttack": "Ryuk", "Industry": "Health", "average_cost": 11}
-#     ]
-#
-#     RansomAttack = [dic["RansomAttack"] for dic in test_data]
-#     Industry = [dic["Industry"] for dic in test_data]
-#     average_cost = [dic["average_cost"] for dic in test_data]
-#
-#     df = pd.DataFrame({
-#         'RansomAttack': RansomAttack,
-#         'Industry': Industry,
-#         'average_cost': average_cost
-#     })
-#     fig = px.bar(df, x='RansomAttack', y='average_cost', color='Industry', barmode='group')
-#     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-#
-#     return render_template('analytics.html', graphJSON=graphJSON)
 
 
 @app.route('/resources')
@@ -107,23 +81,23 @@ def cyber_attack_news():
 
     return render_template('cyber_attack_news.html', news=news)
 
+
 # Add more routes for other news categories
 
 
 @app.route('/analytics')
+@app.route('/analytics')
 def analytics():
-    global df, filtered_df
+    global analysis_results
 
-    # Perform your analysis
-    # analysis_df = analysis.analyze_attack_vectors(filtered_df, 5)
-
-    # Get a dictionary of yearly vulnerabilities dataframes
-    vulnerability_dict = analysis.store_vulnerabilities_in_dict(
-        filtered_df, 'vulnerability', True)
+    # Perform analysis if results are not available
+    if analysis_results is None:
+        vulnerability_dict = analysis.store_vulnerabilities_in_dict(filtered_df, 'vulnerability', True)
+        analysis_results = vulnerability_dict
 
     # Create a list of line traces
     traces = []
-    for vulnerability, df in vulnerability_dict.items():
+    for vulnerability, df in analysis_results.items():
         trace = go.Scatter(
             x=df['cve.published'],
             y=df['Number_of_Vulnerabilities'],
